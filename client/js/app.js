cfdde9d2532311e86476f5a4983fa9907d5a6642
MrFreesher <artur260@op.pl>
2020-01-03 20:37:49 +0100

    Add summary and table with ingredients

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,14 @@
 const search = document.getElementById("search");
 const matchlist = document.getElementById("match-list");
 const addBtn = document.getElementById("addBtn");
-const mealFoods = [];
+const summarySection = document.querySelector("#summary");
+const caloriesSummary = document.querySelector("#caloriesvalue");
+const proteinSummary = document.querySelector("#proteinvalue");
+const fatSummary = document.querySelector("#fatvalue");
+const carbohydratesSummary = document.querySelector("#carbohydratesvalue");
+const ingredientsList = document.querySelector("tbody");
+summarySection.style.visibility = "hidden";
+let mealFoods = [];
 let searchedFood = [];
 const searchFood = async searchText => {
   matchlist.innerHTML = "";
@@ -12,11 +19,9 @@ const searchFood = async searchText => {
   } else {
     searchedFood = [];
   }
-  outputHtml(searchedFood);
-  console.log("Searched food", searchedFood);
+  showSearchedFood(searchedFood);
 };
-const outputHtml = foods => {
-  console.log("Foods", foods);
+const showSearchedFood = foods => {
   const html = foods.map(item => {
     const el = document.createElement("option");
     el.value = item.Name;
@@ -27,7 +32,7 @@ const outputHtml = foods => {
     el.addEventListener("click", addMeal);
     return el;
   });
-  console.log(html);
+
   html.forEach(element => {
     matchlist.appendChild(element);
   });
@@ -37,13 +42,69 @@ const addMeal = e => {
   const searchText = search.value;
   const choosen = searchedFood.filter(item => item.Name === searchText);
   mealFoods.push(...choosen);
-  console.log(mealFoods);
-  // const ing = searchedFood.filter(item => {
-  //   return item.id == ingredient;
-  // });
-  // console.log("ing", ing);
-  // mealFoods.push(ing[0]);
-  // console.log("Mealfood", mealFoods);
+  showFood();
+  showSummary();
+};
+const showSummary = () => {
+  if (mealFoods.length === 0) {
+    summarySection.style.visibility = "hidden";
+  } else {
+    summarySection.style.visibility = "visible";
+    let calories = 0;
+    let protein = 0;
+    let fat = 0;
+    let carbohydrates = 0;
+    for (let i = 0; i < mealFoods.length; i++) {
+      calories += Number.parseInt(mealFoods[i].Calories);
+      protein += Number.parseFloat(mealFoods[i].Protein);
+      fat += Number.parseFloat(mealFoods[i].Fat);
+      carbohydrates += Number.parseFloat(mealFoods[i].Carbohydrates);
+    }
+
+    console.log("Calories", calories);
+    caloriesSummary.textContent = calories;
+    proteinSummary.textContent = protein;
+    fatSummary.textContent = fat;
+    carbohydratesSummary.textContent = carbohydrates;
+  }
+};
+const showFood = () => {
+  const html = mealFoods.map(item => {
+    const el = document.createElement("tr");
+    const nameTd = document.createElement("td");
+    nameTd.textContent = item.Name;
+    const caloriesTd = document.createElement("td");
+    caloriesTd.textContent = item.Calories;
+    const proteinTd = document.createElement("td");
+    proteinTd.textContent = item.Protein;
+    const fatTd = document.createElement("td");
+    fatTd.textContent = item.Fat;
+    const carbohydratesTd = document.createElement("td");
+    carbohydratesTd.textContent = item.Carbohydrates;
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Usuń";
+    removeBtn.classList.add("btn-primary");
+    removeBtn.addEventListener("click", () => removeFood(item.id));
+    el.appendChild(nameTd);
+    el.appendChild(caloriesTd);
+    el.appendChild(proteinTd);
+    el.appendChild(fatTd);
+    el.appendChild(carbohydratesTd);
+    el.appendChild(removeBtn);
+    return el;
+  });
+  ingredientsList.innerHTML = "";
+
+  html.forEach(element => {
+    ingredientsList.append(element);
+  });
+};
+const removeFood = id => {
+  let newList = mealFoods.filter(item => item.id !== id);
+
+  mealFoods = newList;
+  showFood();
+  showSummary();
 };
 search.addEventListener("input", () => searchFood(search.value));
 addBtn.addEventListener("click", addMeal);
